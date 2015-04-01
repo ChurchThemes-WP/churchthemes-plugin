8355286c0f7ad5e20a2dad7aa37fa63c1b79ad7f
Anthony Garand <anthonygarand@gmail.com>
2015-04-01 17:36:09 -0400

    Updates

--- includes/widgets.php
+++ includes/widgets.php
@@ -41,124 +41,61 @@ function ctc_widgets() {
 			'class'						=> 'CTFW_Widget_Categories',	// widget class name
 			'class_file'				=> 'widget-categories.php',		// filename of class in framework class directory
 			'template_file'				=> 'widget-categories.php',		// filename of template in widget-templates directory
-			'ctc_required'				=> false,						// requires Church Theme Content plugin to be active
-			'theme_support'				=> 'ctc-widget-categories',	// add_theme_support() feature required (can be empty)
-			'theme_support_required'	=> array(),						// additional features theme must support for widget to register
-			'icon'						=> 'dashicons-microphone',
-			'unregister' 				=> array(						// widgets to unregister when this is registered
-											'WP_Widget_Categories'
-										),
 		),
 		'ctc-posts' => array(
 			'class'						=> 'CTFW_Widget_Posts',
 			'class_file'				=> 'widget-posts.php',
 			'template_file'				=> 'widget-posts.php',
-			'ctc_required'				=> false,
-			'theme_support'				=> 'ctc-widget-posts',
-			'theme_support_required'	=> array(),
-			'unregister'				=> array(
-				'WP_Widget_Recent_Posts'
-			)
 		),
 		'ctc-sermons' => array(
 			'class'						=> 'CTFW_Widget_Sermons',
 			'class_file'				=> 'widget-sermons.php',
 			'template_file'				=> 'widget-sermons.php',
-			'ctc_required'				=> true,
-			'theme_support'				=> 'ctc-widget-sermons',
-			'theme_support_required'	=> array(
-				'ctc-sermons',
-			),
-			'unregister'				=> array(),
 		),
 		'ctc-events' => array(
 			'class'						=> 'CTFW_Widget_Events',
 			'class_file'				=> 'widget-events.php',
 			'template_file'				=> 'widget-events.php',
-			'ctc_required'				=> true,
-			'theme_support'				=> 'ctc-widget-events',
-			'theme_support_required'	=> array(
-				'ctc-events',
-			),
-			'unregister'				=> array(),
 		),
 		'ctc-gallery' => array(
 			'class'						=> 'CTFW_Widget_Gallery',
 			'class_file'				=> 'widget-gallery.php',
 			'template_file'				=> 'widget-gallery.php',
-			'ctc_required'				=> false, // uses native WordPress galleries
-			'theme_support'				=> 'ctc-widget-gallery',
-			'theme_support_required'	=> array(),
-			'unregister'				=> array(),
 		),
 		'ctc-galleries' => array(
 			'class'						=> 'CTFW_Widget_Galleries',
 			'class_file'				=> 'widget-galleries.php',
 			'template_file'				=> 'widget-galleries.php',
-			'ctc_required'				=> false, // uses native WordPress galleries
-			'theme_support'				=> 'ctc-widget-galleries',
-			'theme_support_required'	=> array(),
-			'unregister'				=> array(),
 		),
 		'ctc-people' => array(
 			'class'						=> 'CTFW_Widget_People',
 			'class_file'				=> 'widget-people.php',
 			'template_file'				=> 'widget-people.php',
-			'ctc_required'				=> true,
-			'theme_support'				=> 'ctc-widget-people',
-			'theme_support_required'	=> array(
-				'ctc-people',
-			),
-			'unregister'				=> array(),
 		),
 		'ctc-locations' => array(
 			'class'						=> 'CTFW_Widget_Locations',
 			'class_file'				=> 'widget-locations.php',
 			'template_file'				=> 'widget-locations.php',
-			'ctc_required'				=> true,
-			'theme_support'				=> 'ctc-widget-locations',
-			'theme_support_required'	=> array(
-				'ctc-locations',
-			),
-			'unregister'				=> array(),
 		),
 		'ctc-archives' => array(
 			'class'						=> 'CTFW_Widget_Archives',
 			'class_file'				=> 'widget-archives.php',
 			'template_file'				=> 'widget-archives.php',
-			'ctc_required'				=> false,
-			'theme_support'				=> 'ctc-widget-archives',
-			'theme_support_required'	=> array(),
-			'unregister' 				=> array(
-											'WP_Widget_Archives'
-										)
 		),
 		'ctc-giving' => array(
 			'class'						=> 'CTFW_Widget_Giving',
 			'class_file'				=> 'widget-giving.php',
 			'template_file'				=> 'widget-giving.php',
-			'ctc_required'				=> false,
-			'theme_support'				=> 'ctc-widget-giving',
-			'theme_support_required'	=> array(),
-			'unregister'				=> array(),
 		),
 		'ctc-slide' => array(
 			'class'						=> 'CTFW_Widget_Slide',
 			'class_file'				=> 'widget-slide.php',
 			'template_file'				=> 'widget-slide.php',
-			'ctc_required'				=> false,
-			'theme_support'				=> 'ctc-widget-slide',
-			'theme_support_required'	=> array(),
-			'unregister'				=> array(),
 		),
 		'ctc-highlight' => array(
 			'class'						=> 'CTFW_Widget_Highlight',
 			'class_file'				=> 'widget-highlight.php',
 			'template_file'				=> 'widget-highlight.php',
-			'ctc_required'				=> false,
-			'theme_support'				=> 'ctc-widget-highlight',
-			'theme_support_required'	=> array(),
-			'unregister'				=> array(),
 		),
 	);
 
@@ -183,215 +120,14 @@ function ctc_register_widgets() {
 	// Available widgets
 	$widgets = ctc_widgets();
 
-	// Church Theme Content plugin is installed and activated?
-	$ctc_active = ctc_plugin_active();
-
 	// Loop widgets
 	foreach ( $widgets as $widget_id => $widget_data ) {
 
-		// Theme supports widget and required features?
-		$supported = true;
-		$theme_support = array_merge(
-			(array) $widget_data['theme_support'],
-			(array) $widget_data['theme_support_required']
-		);
-		foreach ( $theme_support as $feature ) {
-			if ( ! empty( $feature ) && ! current_theme_supports( $feature ) ) { // support can be empt / not required (filtering in a non-framework widget)
-				$supported = false; // one strike and you're out
-				break;
-			}
-		}
-
-		// Theme support is okay
-		if ( $supported ) {
-
-			// Church Theme Content is active or not required for widget
-			if ( empty( $widget_data['ctc_required'] ) || $ctc_active ) {
-
-				// Include class if exists
-				$widget_class_paths = array(
-					trailingslashit( CTFW_WIDGETS_THEME_CLASS_DIR ) . $widget_data['class_file'], // check non-framework dir first in case is theme-provided
-					trailingslashit( CTFW_WIDGETS_CLASS_DIR ) . $widget_data['class_file']
-				);
-				if ( locate_template( $widget_class_paths, true ) ) { // includes and returns true if exists
-
-					// Register the widget
-					register_widget( $widget_data['class'] );
-
-					// Unregister widgets it replaces
-					if ( isset( $widget_data['unregister'] ) ) {
-						foreach ( $widget_data['unregister'] as $unregister_widget ) {
-							unregister_widget( $unregister_widget );
-						}
-					}
-
-				}
-
-			}
-
-		}
+		// Register the widget
+		register_widget( $widget_data['class'] );
 
 	}
 
 }
 
 add_action( 'widgets_init', 'ctc_register_widgets' ); // same as init 1
-
-/*********************************************
- * THEME SUPPORT
- *********************************************/
-
-/**
- * Get theme support data for a widget
- *
- * @since 0.9
- * @param string $widget_id Widget ID
- * @param string $argument Specific argument
- * @return mixed Theme support data
- */
-function ctc_get_widget_theme_support( $widget_id, $argument ) {
-
-	// Null by default so if argument data not found, isset() returns false
-	$data = null;
-
-	// Get widgets
-	$widgets = ctc_widgets();
-
-	// Get widget data
-	if ( isset( $widgets[$widget_id] ) ) { // valid widget
-
-		// Widget data
-		$widget = $widgets[$widget_id];
-
-		// Theme has support for widget
-		$support = get_theme_support( $widget['theme_support'] );
-		if ( $support ) {
-
-			// Get theme support data
-			$support = isset( $support[0] ) && is_array( $support ) ? $support[0] : false;
-
-			// Get fields supported
-			if ( isset( $support[$argument] ) ) { // argument is set (even if empty)
-
-				// Make new array out of fields theme supports
-				$data = $support[$argument];
-
-			}
-
-		}
-
-	}
-
-	// Return filterable
-	return apply_filters( 'ctc_get_widget_theme_support', $data, $widget_id, $argument );
-
-}
-
-/*********************************************
- * FIELD FILTERING
- *********************************************/
-
-/**
- * Filter widget fields
- *
- * Add filters for classes/widget.php to set visibility and override data on fields based on theme support.
- *
- * @since 0.9
- */
-function ctc_filter_widget_fields() {
-
-	// Loop widgets to filter their fields
-	$widgets = ctc_widgets();
-	foreach ( $widgets as $widget_id => $widget_data ) {
-
-		// Set Visible Fields
-		add_filter( 'ctc_widget_visible_fields-' . $widget_id, 'ctc_set_visible_widget_fields', 10, 2 );
-
-		// Set Field Overrides
-		add_filter( 'ctc_widget_field_overrides-' . $widget_id, 'ctc_set_widget_field_overrides', 10, 2 );
-
-	}
-
-}
-
-add_action( 'widgets_init', 'ctc_filter_widget_fields' );
-
-/**
- * Set Visible Fields
- *
- * Show or hide fields for a widget based on add_theme_support.
- * If no fields specifically supported, all are used.
- * If an empty array of fields is passed, no fields are used.
- *
- * @since 0.9
- * @param array $visible_fields Default visible fields
- * @param string $widget_id Widget ID
- * @return array Modified visible fields
- */
-function ctc_set_visible_widget_fields( $visible_fields, $widget_id ) {
-
-	// Get theme's supported fields for widget
-	$supported_fields = ctc_get_widget_theme_support( $widget_id, 'fields' );
-
-	// Check if fields are set (even if empty)
-	if ( isset( $supported_fields ) ) {
-
-		// Make new array out of fields theme supports
-		$visible_fields = $supported_fields;
-
-		// (here could access Church Theme Content plugin settings to override theme's feature support)
-
-	}
-
-	// Return default or filtered field list
-	return $visible_fields;
-
-}
-
-/**
- * Set Field Overrides
- *
- * Override widget field data based on add_theme_support.
- *
- * @since 0.9
- * @param array $field_overrides Field data to override current settings with
- * @param string $widget_id The widget's ID
- * @return array Field override data
- */
-function ctc_set_widget_field_overrides( $field_overrides, $widget_id ) {
-
-	// Return field overrides, if any
-	return ctc_get_widget_theme_support( $widget_id, 'field_overrides' );
-
-}
-
-/*********************************************
- * DATA
- *********************************************/
-
-/**
- * Get all registered widgets
- *
- * Is there a better way to do this?
- *
- * @since 0.9
- * @global array $wp_registered_widgets
- * @return array Registered widgets
- */
-function ctc_get_registered_widgets() {
-
-	global $wp_registered_widgets;
-
-	$widgets = array();
-
-	foreach ( $wp_registered_widgets as $widget ) {
-		if ( ! empty( $widget['callback'][0]->id_base ) ) {
-			$widgets[] = $widget['callback'][0]->id_base;
-		}
-	}
-
-	$widgets = array_unique( $widgets ); // no duplicates
-
-	return apply_filters( 'ctc_get_registered_widgets', $widgets );
-
-}
